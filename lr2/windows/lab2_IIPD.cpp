c899963c5c09a50c6c5e7702d07b7bd7a085404f
danilchenko-alexander <alexandrdanilchenko@mail.ru>
2017-12-01 19:00:12 +0300

    labs

---
+++ lr2/windows/lab2_IIPD.cpp
@@ -0,0 +1,338 @@
+﻿#define _SCL_SECURE_NO_WARNINGS 1
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <Ntddscsi.h>
+#include <stdint.h>
+#include <bitset>
+#include <string>
+#include "winioctl.h"
+#include <mbstring.h>
+
+#ifdef _UNICODE
+typedef wchar_t TCHAR;
+#else
+typedef char TCHAR;
+#endif
+
+#define Name L"\\\\.\\"
+
+#define ATABit 80
+#define DMABit 63
+#define PIOBit 64
+
+#define KB_SIZE 1024
+#define MB_SIZE ((KB_SIZE) * (KB_SIZE))
+#define GB_SIZE ((MB_SIZE) * (KB_SIZE))
+
+CONST UINT bufferSize = 512;
+CONST BYTE identifyDataCommandId = 0xEC;
+
+
+using namespace std;
+
+void getDeviceModel(WORD *data){
+	cout << "DEVICE MODEL: \t";
+	int16_t devicemodel[20];
+	for (int i = 0; i < 20; i++)
+		devicemodel[i] = data[27 + i];
+	for (int i = 0; i < 20; i++)
+	{
+		cout << (char)(devicemodel[i] >> 8);
+		cout << (char)(devicemodel[i]);
+	}
+	cout << endl;
+}
+
+void getDeviceFirmware(WORD *data){
+	cout << "Firmware: \t";
+	int16_t Firmware[4];
+	for (int i = 0; i < 4; i++)
+		Firmware[i] = data[23 + i];
+	for (int i = 0; i < 4; i++)
+	{
+		cout << (char)(Firmware[i] >> 8);
+		cout << (char)(Firmware[i]);
+	}
+	cout << endl;
+}
+
+void getDeviceSerial(WORD *data){
+	cout << "DEVICE SERIAL: \t";
+	int16_t deviceSerial[10];
+	for (int i = 0; i < 10; i++)
+		deviceSerial[i] = data[10 + i];
+	for (int i = 0; i < 10; i++)
+	{
+		cout << (char)(deviceSerial[i] >> 8);
+		cout << (char)(deviceSerial[i]);
+	}
+	cout << endl;
+}
+
+
+BOOL GetDriveGeometry(LPWSTR wszPath, DISK_GEOMETRY *pdg)
+{
+	HANDLE hDevice = INVALID_HANDLE_VALUE;  // handle to the drive to be examined 
+	BOOL bResult = FALSE;                 // results flag
+	DWORD junk = 0;                     // discard results
+
+	hDevice = CreateFileW(wszPath,          // drive to open
+		0,                // no access to the drive
+		FILE_SHARE_READ | // share mode
+		FILE_SHARE_WRITE,
+		NULL,             // default security attributes
+		OPEN_EXISTING,    // disposition
+		0,                // file attributes
+		NULL);            // do not copy file attributes
+
+	if (hDevice == INVALID_HANDLE_VALUE)    // cannot open the drive
+	{
+		return (FALSE);
+	}
+
+	bResult = DeviceIoControl(hDevice,                       // device to be queried
+		IOCTL_DISK_GET_DRIVE_GEOMETRY, // operation to perform
+		NULL, 0,                       // no input buffer
+		pdg, sizeof(*pdg),            // output buffer
+		&junk,                         // # bytes returned
+		(LPOVERLAPPED)NULL);          // synchronous I/O
+	CloseHandle(hDevice);
+	return (bResult);
+}
+
+WORD* GetDeviceIOControl(LPWSTR wszDrive_converted){
+	HANDLE hDevice(::CreateFileW(wszDrive_converted, GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL));
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		std::wcout << L"CreateFileW( " << "here" << L" ) failed.  LastError: " << GetLastError() << std::endl;
+		system("pause");
+		return FALSE;
+	}
+	BOOL bResult = FALSE;
+	DWORD dwBytes;
+	UCHAR identifyDataBuffer[bufferSize
+		+ sizeof(ATA_PASS_THROUGH_EX)] = { 0 };
+
+	ATA_PASS_THROUGH_EX & PTE = *(ATA_PASS_THROUGH_EX *)identifyDataBuffer;
+	PTE.Length = sizeof(PTE);
+	PTE.TimeOutValue = 10;
+	PTE.DataTransferLength = 512;
+	PTE.DataBufferOffset = sizeof(ATA_PASS_THROUGH_EX);
+
+	IDEREGS * ideRegs = (IDEREGS *)PTE.CurrentTaskFile;
+	ideRegs->bCommandReg = identifyDataCommandId;
+	ideRegs->bSectorCountReg = 1;
+
+	PTE.AtaFlags = ATA_FLAGS_DATA_IN | ATA_FLAGS_DRDY_REQUIRED;
+
+	bResult = DeviceIoControl(hDevice, IOCTL_ATA_PASS_THROUGH, &PTE,
+		sizeof(identifyDataBuffer), &PTE,
+		sizeof(identifyDataBuffer), &dwBytes, 0);
+	if (bResult == FALSE) {
+
+		bResult = DeviceIoControl(hDevice, IOCTL_SCSI_PASS_THROUGH, &PTE,
+			sizeof(identifyDataBuffer), &PTE,
+			sizeof(identifyDataBuffer), &dwBytes, 0);
+		if (bResult == FALSE) {
+			std::cout << "Unknown disk device:"
+				<< GetLastError() << std::endl;
+
+		}
+		std::cout << "Something went wrong:"
+			<< GetLastError() << std::endl;
+
+		system("pause");
+		
+	}
+	WORD* data = (WORD *)(identifyDataBuffer + sizeof(ATA_PASS_THROUGH_EX));
+	
+	//showParameters
+
+	//devicemodel
+	getDeviceModel(data);
+	//deviceFirmware
+	getDeviceFirmware(data);
+	//deviceSerial
+	getDeviceSerial(data);
+
+	return data;
+}
+
+std::string wstrtostr(const std::wstring &wstr)
+{
+	// Convert a Unicode string to an ASCII string
+	std::string strTo;
+	char *szTo = new char[wstr.length() + 1];
+	szTo[wstr.size()] = '\0';
+	WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), -1, szTo, (int)wstr.length(), NULL, NULL);
+	strTo = szTo;
+	delete[] szTo;
+	return strTo;
+}
+
+std::wstring strtowstr(const std::string &str)
+{
+	// Convert an ASCII string to a Unicode String
+	std::wstring wstrTo;
+	wchar_t *wszTo = new wchar_t[str.length() + 1];
+	wszTo[str.size()] = L'\0';
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wszTo, (int)str.length());
+	wstrTo = wszTo;
+	delete[] wszTo;
+	return wstrTo;
+}
+
+LPWSTR ConvertToLPWSTR(const std::wstring& s)
+{
+	LPWSTR ws = new wchar_t[s.size() + 1];
+	copy(s.begin(), s.end(), ws);
+	ws[s.size()] = 0;
+	return ws;
+}
+
+
+void getFreeSpaceValues(char pszDrive[])
+{
+	typedef BOOL(WINAPI *P_GDFSE)(LPCTSTR, PULARGE_INTEGER,PULARGE_INTEGER, PULARGE_INTEGER);
+	BOOL test, fResult;
+	unsigned __int64 i64FreeBytesToCaller,
+		i64TotalBytes,
+		i64FreeBytes;
+	P_GDFSE pGetDiskFreeSpaceEx = NULL;
+	pGetDiskFreeSpaceEx = (P_GDFSE)GetProcAddress(
+		GetModuleHandle("kernel32.dll"),
+		"GetDiskFreeSpaceExA");
+	test = GetDiskFreeSpaceEx(pszDrive,
+		(PULARGE_INTEGER)&i64FreeBytesToCaller,
+		(PULARGE_INTEGER)&i64TotalBytes,
+		(PULARGE_INTEGER)&i64FreeBytes);
+	if (test)
+	{
+		printf("FREE SPAE = %d GB;\n",
+			i64FreeBytes / GB_SIZE);
+		printf("BUSY SPACE = %d GB;\n",
+			(i64TotalBytes - i64FreeBytes) / GB_SIZE);
+		printf("TOTAL SPACE = %d GB;\n",
+			i64TotalBytes / GB_SIZE);
+	}
+}
+
+void getAtaSupport(int16_t ataSupportBits){
+	bitset<16> mask(ataSupportBits);
+	string ataSupportStr = "Supported_ATA: ";
+	if ((mask[2]))
+	{
+		ataSupportStr += "ATA/atapi-4,";
+	}
+	if ((mask[3]))
+	{
+		ataSupportStr += "ATA/atapi-5,";
+	}
+	if ((mask[4]))
+	{
+		ataSupportStr += "ATA/atapi-6,";
+	}
+	if ((mask[5]))
+	{
+		ataSupportStr += "ATA/atapi-7,";
+	}
+	if ((mask[6]))
+	{
+		ataSupportStr += "ATA8-ACS";
+	}
+	cout << ataSupportStr << endl;
+}
+
+void getDmaSupport(int16_t dmaSupportBits){
+	bitset<16> maskDma(dmaSupportBits);
+	//std::cout << maskDma << endl;
+	string dmaSupportStr = "Supported_DMA: ";
+	if (maskDma[0])
+	{
+		dmaSupportStr += "MultiDMA-0,";
+	}
+	if (maskDma[1])
+	{
+		dmaSupportStr += "MultiDMA-1,";
+	}
+	if (maskDma[2])
+	{
+		dmaSupportStr += "MultiDMA-2,";
+	}
+	if (maskDma[3])
+	{
+		dmaSupportStr += "MultiDMA-3,";
+	}
+	if (maskDma[4])
+	{
+		dmaSupportStr += "MultiDMA-4,";
+	}
+	if (maskDma[5])
+	{
+		dmaSupportStr += "MultiDMA-5,";
+	}
+	if (maskDma[6])
+	{
+		dmaSupportStr += "MultiDMA-6,";
+	}
+	if (maskDma[7])
+	{
+		dmaSupportStr += "MultiDMA-7";
+	}
+	cout << dmaSupportStr << endl;
+}
+
+void getPIOSupport(int16_t pioSupportBits){
+	bitset<16> maskPio(pioSupportBits);
+	string pioSupportStr = "Supported_PIO: ";
+	if (maskPio[0])
+	{
+		pioSupportStr += "PIO-3,";
+	}
+	if (maskPio[1])
+	{
+		pioSupportStr += "PIO-4";
+	}
+	cout << pioSupportStr << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+		TCHAR szDrive[] = ("\\A:");
+		DWORD uDriveMask = GetLogicalDrives();
+		if (uDriveMask == 0)
+			printf("\nGetLogicalDrives() failed with failure code: %d\n", GetLastError());
+		else
+		{
+			printf("\nThis machine has the following logical drives:\n");
+			while (uDriveMask)
+			{
+				if (uDriveMask & 1){
+					wstring fullName = Name;
+					fullName += strtowstr(szDrive);
+
+					LPWSTR wszDrive_converted = ConvertToLPWSTR(fullName);
+					WORD *data = GetDeviceIOControl(wszDrive_converted);
+
+					int16_t ataSupportBits = data[ATABit];
+					int16_t dmaSupportBits = data[DMABit];
+					int16_t pioSupportBits = data[PIOBit];
+					
+					getFreeSpaceValues("C://");
+					getAtaSupport(ataSupportBits);
+					getDmaSupport(dmaSupportBits);
+					getPIOSupport(pioSupportBits);
+
+					system("pause");
+					return 0;
+				}
+		++szDrive[1];
+		uDriveMask >>= 1;
+			}
+			printf("\n ");
+		}
+}
---
+++ lr4/Laba_4/USB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsbEject;
+
+namespace Laba_4
+{
+    //Generic class for USB devices
+    class Usb
+    {
+        //Name of the device such as D:/
+        public string DeviceName { get; set; }
+        //Free space on disk
+        public string FreeSpace { get; set; }
+        //Used space on disk
+        public string UsedSpace { get; set; }
+        //Total space on disk
+        public string TotalSpace { get; set; }
+        //Mtp device check
+        public bool IsMtpDevice { get; set; }
+
+        public Usb(string name, string freeSize, string usedSize, string totalSize, bool check)
+        {
+            DeviceName = name;
+            FreeSpace = freeSize;
+            UsedSpace = usedSize;
+            TotalSpace = totalSize;
+            IsMtpDevice = check;
+        }
+        //Method for ejecting device with utility RemoveDrive
+        public bool EjectDevice()
+        {
+            var tempName = this.DeviceName.Remove(2);
+            var ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == this.DeviceName.Remove(2));
+            ejectedDevice.Eject(false);
+            ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == tempName);
+            if (ejectedDevice == null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
